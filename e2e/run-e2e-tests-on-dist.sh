2535c6b50aebf5e7825a0bb74f325b0bcba555ef
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-06-23 18:07:59 +0200

    create Dockerfile for app

--- e2e/run-e2e-tests-on-dist.sh
+++ e2e/run-e2e-tests-on-dist.sh
@@ -7,21 +7,33 @@
   set -e
   cd ..
 
-  # start server in dist mode
-  yarn clean
-  yarn build
-  PORT=9999 node dist/index.js &
+  ./docker-build.sh
+
+  docker run \
+    -e PORT=9999 \
+    -e AFFILIATION="org2" \
+    -e MSP_ID="Org2MSP" \
+    -e FABRIC_CA_URL="http://localhost:7032" \
+    -e DISCOVERY_URLS="grpc://localhost:7060,grpc://localhost:7070" \
+    -e AS_LOCALHOST="true" \
+    -p 9998:9999 \
+    -d \
+    --rm \
+    softwaremill/fabrica-rest &
+
   server_pid="$!"
   echo "Started server with PID $server_pid at port $PORT"
   trap "echo 'Stopping server' && kill $server_pid" EXIT SIGINT ERR
   sleep 5
 
   # run tests
-  PORT=9999 \
+  PORT=9998 \
     AFFILIATION="org2" \
     MSP_ID="Org2MSP" \
     FABRIC_CA_URL="http://localhost:7032" \
     DISCOVERY_URLS="grpc://localhost:7060,grpc://localhost:7070" \
     AS_LOCALHOST="true" \
-    yarn test-ci e2e
+    npm run test-e2e
 )
+
+  TODO stop docker
