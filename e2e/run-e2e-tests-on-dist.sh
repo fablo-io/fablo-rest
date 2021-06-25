f05cf51effe9772275e333cffb3e20248d5bd0d3
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-06-25 06:10:49 +0200

    update script to run tests

--- e2e/run-e2e-tests-on-dist.sh
+++ e2e/run-e2e-tests-on-dist.sh
@@ -7,8 +7,19 @@
   set -e
   cd ..
 
+  #
+  # Build Docker image
+  #
   ./docker-build.sh
 
+  #
+  # Start application in the container.
+  #
+  # Note: it works only on linux and won't work on OSX. We cannot use host.docker.internal here,
+  # we need localhost to refer to the host where fabric network is set up.
+  #
+  container=fabrica_rest_test
+  port=8000
   docker run \
     -e PORT=9999 \
     -e AFFILIATION="org2" \
@@ -16,18 +27,21 @@
     -e FABRIC_CA_URL="http://localhost:7032" \
     -e DISCOVERY_URLS="grpc://localhost:7060,grpc://localhost:7070" \
     -e AS_LOCALHOST="true" \
-    -p 9998:9999 \
+    -p "$port:9999" \
+    --network="host" \
     -d \
     --rm \
-    softwaremill/fabrica-rest &
+    --name "$container" \
+    softwaremill/fabrica-rest
 
-  server_pid="$!"
-  echo "Started server with PID $server_pid at port $PORT"
-  trap "echo 'Stopping server' && kill $server_pid" EXIT SIGINT ERR
+  echo "Started server on container $container at port $port"
+  trap "echo 'Stopping container $container' && docker stop $container" EXIT SIGINT ERR
   sleep 5
 
-  # run tests
-  PORT=9998 \
+  #
+  # Run tests
+  #
+  PORT=8000 \
     AFFILIATION="org2" \
     MSP_ID="Org2MSP" \
     FABRIC_CA_URL="http://localhost:7032" \
@@ -35,5 +49,3 @@
     AS_LOCALHOST="true" \
     npm run test-e2e
 )
-
-  TODO stop docker
