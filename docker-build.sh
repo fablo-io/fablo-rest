61618bbcf35434ef18a99511862d73eaa36129bc
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-09-17 12:33:34 +0200

    The Great Renaming

--- docker-build.sh
+++ docker-build.sh
@@ -2,18 +2,18 @@
 
 set -euo
 
-FABRICA_REST_HOME="$(cd "$(dirname "$0")" && pwd)"
-FABRICA_REST_VERSION=$(jq -r '.version' <"$FABRICA_REST_HOME/package.json")
-DOCKER_IMAGE_BASE_NAME="softwaremill/fabrica-rest"
-DOCKER_IMAGE_TAG="$DOCKER_IMAGE_BASE_NAME:$FABRICA_REST_VERSION"
+FABLO_REST_HOME="$(cd "$(dirname "$0")" && pwd)"
+FABLO_REST_VERSION=$(jq -r '.version' <"$FABLO_REST_HOME/package.json")
+DOCKER_IMAGE_BASE_NAME="softwaremill/fablo-rest"
+DOCKER_IMAGE_TAG="$DOCKER_IMAGE_BASE_NAME:$FABLO_REST_VERSION"
 
 echo "Building new image..."
-echo "   FABRICA_REST_HOME:    $FABRICA_REST_HOME"
-echo "   FABRICA_REST_VERSION: $FABRICA_REST_VERSION"
+echo "   FABLO_REST_HOME:    $FABLO_REST_HOME"
+echo "   FABLO_REST_VERSION: $FABLO_REST_VERSION"
 echo "   DOCKER_IMAGE_TAG:     [$DOCKER_IMAGE_BASE_NAME, $DOCKER_IMAGE_TAG]"
 
 npm install --silent
 npm run build
 
-docker build --tag "$DOCKER_IMAGE_TAG" "$FABRICA_REST_HOME"
+docker build --tag "$DOCKER_IMAGE_TAG" "$FABLO_REST_HOME"
 docker tag "$DOCKER_IMAGE_TAG" "$DOCKER_IMAGE_BASE_NAME"
